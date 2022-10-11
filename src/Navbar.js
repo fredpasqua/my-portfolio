90ac357f55bad2bb859315e16dcf5dfcc61ea61b
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-10-10 20:33:01 -0400

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,18 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import "./App.css";
 
 function BasicExample() {
   return (
     <Navbar bg="light" variant="light" expand="lg">
-      <Container>
+      <Container className="navbar-container">
         <Navbar.Brand href="#home">Fred Pasqua Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">About</Nav.Link>
             <Nav.Link href="#link">Projects</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>

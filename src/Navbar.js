280413785ce9864be28aec8379865754d2d7a466
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-10-19 23:40:30 -0400

    add fonts to navbar and card components

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,18 +4,20 @@ import Navbar from "react-bootstrap/Navbar";
 import "./Navbar.css";
 function Menu() {
   return (
-    <Navbar bg="light" variant="light" expand="lg">
-      <Container className="navbar-container">
-        <Navbar.Brand href="#home">Fred Pasqua Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">About</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="navstyle">
+      <Navbar bg="light" variant="light" expand="lg">
+        <Container className="navbar-container">
+          <Navbar.Brand href="#home">Fred Pasqua Portfolio</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">About</Nav.Link>
+              <Nav.Link href="#link">Projects</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 

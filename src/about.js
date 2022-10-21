2f84a57fea1e05d383614db8e51384efe5f80573
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-10-20 22:53:00 -0400

    style

--- src/about.js
+++ src/about.js
@@ -4,7 +4,7 @@ class Test extends Component {
   render() {
     const myStyle = {
       backgroundImage: `url(https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676&q=80)`,
-      height: "200px",
+      height: "275px",
       // maxWidth: "700px",
       // marginLeft: "auto",
       // marginRight: "auto",

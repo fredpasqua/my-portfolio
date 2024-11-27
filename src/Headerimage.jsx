b39b5cf5ddec05604a19fd31231cff9d2cd9c992
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-11-27 07:45:40 -0500

    added smaller image to header

--- src/Headerimage.jsx
+++ src/Headerimage.jsx
@@ -1,15 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import deskImage from "./Images/officeDesk.jpg";
 
-class Headerimage extends Component {
-  render() {
-    const myStyle = {
-      backgroundImage: `url(https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676&q=80)`,
-      height: "275px",
-      backgroundSize: "100%",
-      backgroundRepeat: "no-repeat",
-    };
-    return <div style={myStyle}></div>;
-  }
+function Headerimage() {
+  return (
+    <img
+      className="headertop"
+      src={deskImage}
+      alt="desk top with computer and coffee cup"
+    />
+  );
 }
 
 export default Headerimage;
+// class Headerimage extends Component {
+//   render() {
+//     const myStyle = {
+//       backgroundImage: `url(https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676&q=80)`,
+//       height: "275px",
+//       backgroundSize: "100%",
+//       backgroundRepeat: "no-repeat",
+//     };
+//     return <div style={myStyle}></div>;
+//   }
+// }
+
+// export default Headerimage;

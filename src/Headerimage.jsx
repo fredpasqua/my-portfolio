50ad601a813fba636e38977fef3a044064d74f03
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-11-27 07:45:45 -0500

    styling

--- src/Headerimage.jsx
+++ src/Headerimage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import deskImage from "./Images/officeDesk.jpg";
 
 function Headerimage() {
+  //TODO: styling for multiple screens
   return (
     <img
       className="headertop"
@@ -12,16 +13,3 @@ function Headerimage() {
 }
 
 export default Headerimage;
-// class Headerimage extends Component {
-//   render() {
-//     const myStyle = {
-//       backgroundImage: `url(https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676&q=80)`,
-//       height: "275px",
-//       backgroundSize: "100%",
-//       backgroundRepeat: "no-repeat",
-//     };
-//     return <div style={myStyle}></div>;
-//   }
-// }
-
-// export default Headerimage;

62c82cedbcf7bedcb8dcb8feccf800e39b843ce5
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-11-28 09:38:20 -0500

    Revert "some other stuff"
    This reverts commit b0985df2d3c35ff802ae6625ac81b0f9d3d0cde3.

--- src/Tabs/Tabs.jsx
+++ src/Tabs/Tabs.jsx
@@ -5,17 +5,17 @@ function Tabs({ handleClick }) {
   return (
     <ul className="objectSelectorList">
       <li className="text">
-        <button onMouseOver={() => handleClick(1)} className="selectorButton">
+        <button onClick={() => handleClick(1)} className="selectorButton">
           Proj.
         </button>
       </li>{" "}
       <li className="text">
-        <button onMouseOver={() => handleClick(2)} class="selectorButton">
+        <button onClick={() => handleClick(2)} class="selectorButton">
           Notes
         </button>
       </li>{" "}
       <li className="text">
-        <button onMouseOver={() => handleClick(3)} class="selectorButton">
+        <button onClick={() => handleClick(3)} class="selectorButton">
           Tech
         </button>
       </li>{" "}

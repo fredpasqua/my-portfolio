3a3495af4507c4323ecd4ec3e933bf1454143810
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-11-29 09:12:35 -0500

    directory cleanup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { About } from "./About/About";
 import { Projects } from "./Projects/Projects.jsx";
 import { Contact } from "./Contact/Contact.jsx";
 import Menu from "./Navbar/Navbar.jsx";
-import Headerimage from "./Headerimage";
+import Headerimage from "./Headerimage/Headerimage.jsx";
 
 function App() {
   return (

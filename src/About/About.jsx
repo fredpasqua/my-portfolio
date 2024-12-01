b0d993360919315c1c2802faf979ef9289ecb8cf
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-12-01 11:12:58 -0500

    update about section format

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,21 +2,23 @@ import Resume from "../Images/Fred_Pasqua_Resume_2.19.23.pdf";
 export const About = () => (
   <div className="about" id="about">
     <h2 className="header-title">About</h2>
+    <div className="about-lists">
+    
+      <ul>
+        <lh>Tech Skills</lh>
+        <li>Frameworks: React, Angular and React Native</li>
+        <li>Javascript, HTML, CSS, SCSS</li>
+        <li> Node.js, APIs, Axios, Postman</li>
+        <li>Documenting, Github, MongoDB</li>
+      </ul>
+      <ul className="skills">
+        <lh >Related Skills</lh>
+        <li>Copywriting</li>
+        <li>Seach Engine Optimization</li>
+        <li>Sales/Marketing Experience</li>
+      </ul>
+    </div>
 
-    <ul>
-      <li className="bold-li">Tech Skills</li>
-      <li>Frameworks: React, Angular and React Native</li>
-      <li>Javascript, HTML, CSS, SCSS</li>
-      <li> Node.js, APIs, Axios, Postman, Unit and Integration testing</li>
-      <li>Documenting, Github, MongoDB</li>
-      <li className="bold-li">Related Skills</li>
-      <li>Experienced Blogger with SEO Skills</li>
-      <li>Attention to Detail</li>
-      <li>Entrepreneurial Spirit</li>
-      <li>Organized and Efficient</li>
-      <li>Professional Sales Experience</li>
-      <li>Client Focused and Business Centric</li>
-    </ul>
     <div className="neonText">
       <a href={Resume} download="Fred_Pasqua_Resume">
         *** RESUME.PDF ***

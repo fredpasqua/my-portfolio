40ff6d0ac1e67aefc6b803537e5f38f893bb9dc1
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-12-01 11:29:59 -0500

    update neon

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -3,25 +3,30 @@ export const About = () => (
   <div className="about" id="about">
     <h2 className="header-title">About</h2>
     <div className="about-lists">
+      <div>
+        <ul className="skills">
+          <lh>Tech Skills</lh>
+          <li>Frameworks: React, Angular</li>
+          <li>Javascript, HTML, CSS, SCSS</li>
+          <li> Node.js, APIs, Axios, Postman</li>
+          <li>Documenting, Github, MongoDB</li>
+        </ul>
+      </div>
+      <div>
+        <ul className="skills">
+          <lh>Related Skills</lh>
+          <li>Copywriting</li>
+          <li>Seach Engine Optimization</li>
+          <li>Sales and Marketing</li>
+          <li>Project Management</li>
+        </ul>
+      </div>
     
-      <ul>
-        <lh>Tech Skills</lh>
-        <li>Frameworks: React, Angular and React Native</li>
-        <li>Javascript, HTML, CSS, SCSS</li>
-        <li> Node.js, APIs, Axios, Postman</li>
-        <li>Documenting, Github, MongoDB</li>
-      </ul>
-      <ul className="skills">
-        <lh >Related Skills</lh>
-        <li>Copywriting</li>
-        <li>Seach Engine Optimization</li>
-        <li>Sales/Marketing Experience</li>
-      </ul>
     </div>
 
     <div className="neonText">
       <a href={Resume} download="Fred_Pasqua_Resume">
-        *** RESUME.PDF ***
+       Download Resume
       </a>
     </div>
   </div>
